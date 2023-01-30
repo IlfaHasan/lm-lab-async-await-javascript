f673b8f7acdc2dd15c48d4d9c8a42e2ad87b3055
ilfa <ilfahassan@gmail.com>
2023-01-30 23:54:48 +0000

    Exercises done

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,25 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
+/*const fetchData = (apiEndPoint) => {
   fetch(apiEndPoint)
     .then((response) => response.json())
     .then((json) => console.log(json))
     .catch((error) => console.log(error));
+};*/
+
+const fetchDataWithAsync = async (apiEndPoint) => {
+  try {
+    const response = await fetch(apiEndPoint);
+    //console.log(response);
+    const responseJson = await response.json();
+    console.log(responseJson);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-fetchData(jsonTypicode);
+const jsonCat="https://api.thecatapi.com/v1/images/0XYvRd7oD";
+
+fetchDataWithAsync(jsonTypicode);
+fetchDataWithAsync(jsonCat); //get data
